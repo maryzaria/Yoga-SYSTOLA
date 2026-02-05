7c85625a51516859af3cbe459635e0ba2df515b0
Mariia Zaripova <zaripova.mary.m@gmail.com>
2026-02-05 19:37:18 +0400

    resert passwor fixes

--- systola/urls.py
+++ systola/urls.py
@@ -25,6 +25,26 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="account/email_login.html"),
         name="email_login",
     ),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("accounts/", include("allauth.urls")),
     path("", include("core.urls")),
 ]
